5305781df1f7f8b3d0cfd4bd8ebf0ec730111bbb
Pavel Chirko <pasacirko@gmail.com>
2025-07-07 14:35:34 +0400

    add test widget

--- src/processing.py
+++ src/processing.py
@@ -7,20 +7,3 @@ def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[d
     :return: Отфильтрованный список словарей
     """
     return [transaction for transaction in transactions if transaction.get('state') == state]
-
-data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
-# Фильтрация по умолчанию (EXECUTED)
-print(filter_by_state(data))
-# Вывод: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
-# Фильтрация по CANCELED
-print(filter_by_state(data, 'CANCELED'))
-# Вывод: [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
