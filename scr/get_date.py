00ee7fe10aa0511a8aafec823ab6e3beda9fdb19
Pavel Chirko <pasacirko@gmail.com>
2025-07-07 13:39:58 +0400

    fixed get_date

--- scr/get_date.py
+++ scr/get_date.py
@@ -1,9 +1,5 @@
-def get_date(date_str: str) -> str:
+def get_date(date_time: str) -> str:
     """Преобразует дату"""
 
-    # отделяем дату от времени
-    date_part = date_str.split("T")[0]
-    # разбиваем на компоненты
-    year, month, day = date_part.split("-")
-    # собираем в нужном формате
-    return f"{day}.{month}.{year}"
+    times = date_time[:date_time.find("T")].split("-")
+    return ".".join(times)
